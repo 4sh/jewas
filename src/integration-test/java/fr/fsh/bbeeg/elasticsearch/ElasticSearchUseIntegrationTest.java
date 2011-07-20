b180144dcb948ab7a4f447eedb48077084984c4a
Frédéric Camblor <fcamblor@gmail.com>
2011-07-20 17:02:35 +0200

    added search & get use cases in the test

--- src/integration-test/java/fr/fsh/bbeeg/elasticsearch/ElasticSearchUseIntegrationTest.java
+++ src/integration-test/java/fr/fsh/bbeeg/elasticsearch/ElasticSearchUseIntegrationTest.java
@@ -1,20 +1,23 @@
 package fr.fsh.bbeeg.elasticsearch;
 
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.common.xcontent.XContentFactory;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeBuilder;
-import static org.hamcrest.CoreMatchers.*;
-import org.junit.After;
-import static org.junit.Assert.*;
-import org.junit.Before;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,35 +27,55 @@ import java.util.Date;
  */
 public class ElasticSearchUseIntegrationTest {
 
-    private Client client = null;
+    private static Client client = null;
 
-    @Before
-    public void start(){
+    @BeforeClass
+    public static void start() {
         // It is deliberate we don't use the nodeBuild (with local() mode) here (used generally in unit tests)
         // We want to remotely contact an elastic search server started during the integration test phase
+
         client = new TransportClient().addTransportAddress(new InetSocketTransportAddress("localhost", 9300));
+        //client = NodeBuilder.nodeBuilder().node().client();
     }
 
-    @After
-    public void stop(){
+    @AfterClass
+    public static void stop() {
         client.close();
         client = null;
     }
 
     @Test
     public void creationAndRetrievementUseCase() throws IOException {
-        IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
-            .setSource(XContentFactory.jsonBuilder()
-                    .startObject()
-                    .field("user", "kimchy")
-                    .field("postDate", new Date())
-                    .field("message", "trying out Elastic Search")
-                    .endObject()
-            )
-            .execute()
-            .actionGet();
+        IndexResponse iResponse = client.prepareIndex("twitter", "tweet", "1")
+                .setSource(XContentFactory.jsonBuilder()
+                        .startObject()
+                        .field("user", "kimchy")
+                        .field("postDate", new Date())
+                        .field("message", "trying out Elastic Search")
+                        .endObject()
+                )
+                .execute()
+                .actionGet();
 
-        assertThat(response, is(notNullValue()));
-        assertThat(response.id(), is(equalTo("1")));
+        assertThat(iResponse, is(notNullValue()));
+        assertThat(iResponse.id(), is(equalTo("1")));
+
+        SearchResponse sResponse = client.prepareSearch("twitter")
+                .setSearchType(SearchType.DFS_QUERY_AND_FETCH)
+                .setScroll("10m")
+                .setQuery(QueryBuilders.termQuery("user", "kimchy"))
+                .setFrom(0).setSize(60).setExplain(true)
+                .execute()
+                .actionGet();
+
+        assertThat(sResponse, is(notNullValue()));
+        assertThat(sResponse.getHits().hits()[0].getSource().get("message"), is(notNullValue()));
+        assertThat(sResponse.getHits().hits()[0].getSource().get("message").toString(), is(equalTo("trying out Elastic Search")));
+
+
+        GetResponse gResponse = client.prepareGet("twitter", "tweet", "1").execute().actionGet();
+        assertThat(gResponse, is(notNullValue()));
+        assertThat(gResponse.getSource().get("message").toString(), is(equalTo("trying out Elastic Search")));
     }
+
 }
