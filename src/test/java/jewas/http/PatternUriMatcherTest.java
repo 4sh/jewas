45ce17f2007f5bc790dafa390471ac9aa0b8e38f
Frédéric Camblor <fcamblor@gmail.com>
2011-07-25 18:05:57 +0200

    added more basic tests on PatternUrlMatcherTest

--- src/test/java/jewas/http/PatternUriMatcherTest.java
+++ src/test/java/jewas/http/PatternUriMatcherTest.java
@@ -11,10 +11,26 @@ import static org.junit.Assert.*;
 public class PatternUriMatcherTest {
 
     @Test
-    public void shouldLastParameterRetrieve() {
-        Parameters params = new PatternUriPathMatcher("/path/[param1]/[param2]").match("/path/val1/val2/val3");
+    public void shouldBasicTestsOnPatternUriMatcherBeOk() {
+        Parameters params = new PatternUriPathMatcher("/user/[param1]/edit").match("/user/1/edit");
+        assertThat(params, is(notNullValue()));
+        assertThat(params.val("param1"), is(equalTo("1")));
+    }
+
+    @Test
+    public void shouldOptionalParametersDontBreakThePatternUriMatcher() {
+        Parameters params = new PatternUriPathMatcher("/[param1]/[param2]").match("/val1");
         assertThat(params, is(notNullValue()));
         assertThat(params.val("param1"), is(equalTo("val1")));
-        assertThat(params.val("param2"), is(equalTo("val2/val3")));
+        assertThat(params.val("param2"), is(equalTo("")));
     }
+
+    @Test
+    public void shouldLastParameterBeRetrievedEvenIfItContainsSlashes() {
+        Parameters params = new PatternUriPathMatcher("/path/[param1]/[param2]").match("/path/val1/val2/val3/val4");
+        assertThat(params, is(notNullValue()));
+        assertThat(params.val("param1"), is(equalTo("val1")));
+        assertThat(params.val("param2"), is(equalTo("val2/val3/val4")));
+    }
+
 }
