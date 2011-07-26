4fd6566d4154ce490ffbd160e7165bd536ec43ba
Frédéric Camblor <fcamblor@gmail.com>
2011-07-27 00:01:42 +0200

    Added SimpleHtmlRoute that allows to define simple route that will forward to a freemarker template without doing anything else

---
+++ src/main/java/jewas/routes/SimpleHtmlRoute.java
@@ -0,0 +1,30 @@
+package jewas.routes;
+
+import jewas.http.*;
+import jewas.template.Templates;
+
+/**
+ * @author fcamblor
+ */
+public class SimpleHtmlRoute extends AbstractRoute {
+
+    private String templatePath;
+
+    public SimpleHtmlRoute(String uri, String templatePath) {
+        this(HttpMethodMatcher.ALL, new PatternUriPathMatcher(uri), templatePath);
+    }
+
+    public SimpleHtmlRoute(HttpMethodMatcher methodMatcher, UriPathMatcher pathMatcher, String templatePath) {
+        super(methodMatcher, pathMatcher);
+        this.templatePath = templatePath;
+    }
+
+    protected RequestHandler onMatch(HttpRequest request, Parameters parameters) {
+        return new RequestHandler() {
+            @Override
+            public void onRequest(HttpRequest request) {
+                request.respondHtml().content(Templates.process(templatePath, null));
+            }
+        };
+    }
+}
