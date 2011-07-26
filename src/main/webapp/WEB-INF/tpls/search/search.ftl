48d280cf2bfa01cf317d62f8542c84501e4ff079
Frédéric Camblor <fcamblor@gmail.com>
2011-07-26 19:59:37 +0200

    implemented simple search on clientside

--- src/main/webapp/WEB-INF/tpls/search/search.ftl
+++ src/main/webapp/WEB-INF/tpls/search/search.ftl
@@ -1,4 +1,32 @@
 <#include "../common/mainTemplate.ftl">
-<@mainTemplate title="Ecran d'accueil" scripts=[]>
-   Hello world !
+
+<#if compressedJS == "true">
+    <#assign chosenJS = "/public/js/chosen/chosen.jquery.min.js">
+    <#else>
+        <#assign chosenJS = "/public/js/chosen/chosen.jquery.js">
+</#if>
+
+<@mainTemplate title="Ecran d'accueil" scripts=[chosenJS, "/public/js/bbeeg/search/search.js"]>
+<script>
+    $(function() {
+        $("#searchComponent").accordion();
+        SearchQuery.registerClickableForSimpleSearch($("#simpleSearchButton"), $("#simpleSearchQuery"), $("#searchResultsComponent"));
+        SearchQuery.registerClickableForAdvancedSearch($("#advancedSearchButton"), /*$("#simpleSearchQuery")[0], */$("#searchResultsComponent"));
+    });
+</script>
+
+<div id="searchComponent">
+    <h3><a href="#">Recherche simple</a></h3>
+
+    <div>Recherche : <input id="simpleSearchQuery" type="text" name="query"/>
+        <input id="simpleSearchButton" type="submit" value="Rechercher"/></div>
+    <h3><a href="#">Recherche avancée</a></h3>
+
+    <div>Blah blah blah
+        <input id="advancedSearchButton" type="submit" value="Rechercher"/></div>
+</div>
+
+<div id="searchResultsComponent">
+    No results found yet !
+</div>
 </@mainTemplate>
---
+++ src/main/webapp/public/js/bbeeg/search/search.js
@@ -0,0 +1,48 @@
+function SearchQuery() {
+
+    (function() {
+    }).call(this);
+
+    this.simpleContentSearch = function(uniqueSearchField, resultElement) {
+        return search("/content/search", { q: uniqueSearchField.val() }, resultElement);
+    };
+
+    this.advancedContentSearch = function(resultElement/* TODO */) {
+        alert('Not yet implemented :-)');
+    };
+
+    /**
+     * Will perform a search and return a ContentSearchResult[]
+     * @param resourceUrl REST URL returning an array of search results
+     * @param parameters Query parameters given to the resourceUrl
+     */
+    function search(resourceUrl, parameters, resultElement) {
+        $.get(resourceUrl, parameters, function(data) {
+            displayInto(resultElement, data);
+        }, "json");
+    }
+
+    ;
+
+    function displayInto(resultElement, results) {
+        // TODO: refactor this...
+        $('<ul>').appendTo(resultElement);
+        $.each(results, function(index, res) {
+            $('<li>' + res.title + '</li>').appendTo(resultElement);
+        });
+        $('</ul>').appendTo(resultElement);
+    }
+
+    ;
+}
+SearchQuery.registerClickableForSimpleSearch = function(clickableElement, uniqueSearchField, resultElement) {
+    clickableElement.click(function() {
+        new SearchQuery().simpleContentSearch(uniqueSearchField, resultElement);
+    });
+};
+SearchQuery.registerClickableForAdvancedSearch = function(clickableElement /*, TODO */, resultElement) {
+    clickableElement.click(function() {
+        new SearchQuery().advancedContentSearch(resultElement);
+    });
+};
+
