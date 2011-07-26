0f41a7b1e99d7f29c906954a68c05afac9a69b7f
Frédéric Camblor <fcamblor@gmail.com>
2011-07-26 20:15:30 +0200

    using RestServerFactory instead of initializing manually RestServerFactory (will benefif of default handler implementation) + Fixed DRO bouletasse (forgot to commit GetLastAddedContentRoute)

--- src/main/java/fr/fsh/bbeeg/Main.java
+++ src/main/java/fr/fsh/bbeeg/Main.java
@@ -2,44 +2,21 @@ package fr.fsh.bbeeg;
 
 import fr.fsh.bbeeg.routes.GetDashboardRoute;
 import fr.fsh.bbeeg.routes.GetSearchRoute;
-import fr.fsh.bbeeg.routes.GetLastAddedContentRoute;
-import jewas.http.HttpRequest;
-import jewas.http.RequestHandler;
 import jewas.http.RestServer;
-import jewas.http.Route;
-import jewas.http.connector.netty.NettyHttpConnector;
+import jewas.http.RestServerFactory;
 import jewas.routes.StaticResourceRoute;
 
 /**
- * Created by IntelliJ IDEA.
- * User: driccio
- * Date: 22/07/11
- * Time: 16:04
- * To change this template use File | Settings | File Templates.
+ * @author driccio, fcamblor
  */
 public class Main {
     public static void main(String[] args) {
-        final RestServer rs = new RestServer(new NettyHttpConnector())
-                .addRoutes(
-                        new StaticResourceRoute(),
-                        new GetDashboardRoute(),
-                        new GetSearchRoute(),
-                        new GetLastAddedContentRoute()
-                );
-        rs.addHandler(new RequestHandler() {
-            @Override
-            public void onRequest(HttpRequest request) {
-                RequestHandler match = null;
-                for (Route r : rs.routes()) {
-                    if ((match = r.match(request)) != null) {
-                        match.onRequest(request);
-                        return;
-                    }
-                }
-            }
-        })
-                .bind(8086)
-                .start();
+        final RestServer rs = RestServerFactory.createRestServer(8086);
+        rs.addRoutes(
+                new StaticResourceRoute(),
+                new GetDashboardRoute(),
+                new GetSearchRoute()
+        ).start();
         System.out.println("Ready, if you are");
     }
 
