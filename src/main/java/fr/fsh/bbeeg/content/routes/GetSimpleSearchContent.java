58000782aecfdd4873e679336ad72ab918315bc5
Frédéric Camblor <fcamblor@gmail.com>
2011-07-28 12:13:28 +0200

    provided Search query object + put some mocked easter egg in search results

--- src/main/java/fr/fsh/bbeeg/content/routes/GetSimpleSearchContent.java
+++ src/main/java/fr/fsh/bbeeg/content/routes/GetSimpleSearchContent.java
@@ -14,7 +14,22 @@ public class GetSimpleSearchContent extends AbstractRoute {
         super(HttpMethodMatcher.GET, new PatternUriPathMatcher("/content/search"));
     }
 
+    // FIXME : to externalize into resources package (when ContentQueryObject will have been renamed)
+    public static class SearchQueryObject {
+        private String query;
+
+        public SearchQueryObject query(String _query) {
+            this.query = _query;
+            return this;
+        }
+
+        public String query() {
+            return this.query;
+        }
+    }
+
     protected RequestHandler onMatch(HttpRequest request, Parameters parameters) {
+        final SearchQueryObject query = toQueryObject(parameters, SearchQueryObject.class);
         return new RequestHandler() {
             @Override
             public void onRequest(HttpRequest request) {
@@ -22,6 +37,10 @@ public class GetSimpleSearchContent extends AbstractRoute {
 
                 results.add(new ContentSearchResult().id("1").author("fcamblor").title("Premier contenu").mediaType("audio"));
                 results.add(new ContentSearchResult().id("2").author("fcamblor").title("Second contenu").mediaType("video"));
+
+                if (query.query().contains("easterEgg")) {
+                    results.add(new ContentSearchResult().id("3").author("fcamblor").title("Troisieme contenu (caché)").mediaType("eeg"));
+                }
 
                 request.respondJson().object(results);
             }
