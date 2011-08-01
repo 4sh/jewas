e68898125f0d2fc1e602f39aad9b087a52c7bc1d
Frédéric Camblor <fcamblor@gmail.com>
2011-08-01 09:15:16 +0200

    Implemented ChainedSelect in add mode only (deletion will come soon..)

--- src/main/webapp/WEB-INF/tpls/search/search.ftl
+++ src/main/webapp/WEB-INF/tpls/search/search.ftl
@@ -6,16 +6,11 @@
         <#assign chosenJS = "/public/js/chosen/chosen.jquery.js">
 </#if>
 
-<@mainTemplate title="Ecran d'accueil" selectedMenuItem="search" scripts=[chosenJS, "/public/js/bbeeg/search/search.js"] stylesheets=["/public/css/chosen/chosen.css"]>
-
-<script id="authorItemTemplate" type="text/html">
-    <option value="{{= id}}"> {{= name}} </option>
-</script>
-
-<script id="contentTypeItemTemplate" type="text/html">
-    <option value="{{= id}}"> {{= title}} </option>
-</script>
-
+<@mainTemplate
+    title="Ecran d'accueil"
+    selectedMenuItem="search"
+    scripts=[chosenJS, "/public/js/bbeeg/search/search.js", "/public/js/bbeeg/common/widgets/chainedSelect.js"]
+    stylesheets=["/public/css/chosen/chosen.css"]>
 <script>
     function loadAuthors() {
         $.getJSON(
@@ -65,7 +60,8 @@
 				dates.not( this ).datepicker( "option", option, date );
 			}
 		});
-
+        $("#adSearchType").chosen();
+        $("#adSearchCriterias").chosen();
         SearchQuery.bindSearchToForm(
                 new SearchQuery.SearchContext().targetForm($("#simpleSearchForm"))
                         .resultElement($("#searchResultsComponent"))
@@ -74,6 +70,15 @@
                         .selectorForClickableOfSearchNext("#searchNext")
                         .selectorWhereResultsWillBeAppended("#contentResults")
         );
+        var config = new ChainedSelect.Configuration()
+                .ajaxUrlsPerDepth([
+                      "/content/criterias?depth=0",
+                      "/content/criterias?depth=1&parent={value}",
+                      "/content/criterias?depth=2&parent={value}"
+                ]).targetFieldForSelectedOption($("#criterias"))
+                .displaySelectionTarget($("#criteriasList"))
+                .selectMenuContainer($("#searchCriteriaMenuContainer"));
+        var criteriaCombo = new ChainedSelect(config).decorateSelectFieldWithChainedSelectConfiguration($("#adSearchCriterias"));
     });
 </script>
 
@@ -97,13 +102,31 @@
                 <input type="text" id="from" name="from"/>
                 <label for="to">et</label>
                 <input type="text" id="to" name="to"/>
-            </div>
-
-            <div style="width: 600px; display:block;">
+            </div>                                                                                                    id="adSearchDate"
+                                                                                                    name="date"/></div>
+            <div style="width: 600px; display:block; clear:both;">
                 <div style="float:left"><label for="adSearchType">Types de contenu</label> :</div>
                 <div style="float:left">
                     <select id="adSearchType" class="chzn-select side-by-side clearfix" multiple style="width: 350px">
                     </select>
+                </div>
+            </div>
+            <div style="width: 600px; display:block; clear:both;">
+                <div style="float:left"><label for="adSearchCriterias">Critères </label> :</div>
+                <div style="float:left">
+                    <input type="hidden" id="criterias" name="criterias" value="" />
+                    <div class="chzn-container" style="float:left">
+                        <ul class="chzn-choices" id="criteriasList"
+                                style="background-color: transparent; border: 0px; background-image: none;">
+                        </ul>
+                    </div>
+                    <div id="searchCriteriaMenuContainer" style="float:left; padding-left: 10px">
+                        <select id="adSearchCriterias" style="width: 150px">
+                            <option value="Critere 1">Libellé Critere 1</option>
+                            <option value="Critere 2">Libellé Critere 2</option>
+                            <option value="Critere 3">Libellé Critere 3</option>
+                        </select>
+                    </div>
                 </div>
             </div>
             <div style="display:block; clear: both;"><label for="adSearchAuthor">Auteur</label> : <select
@@ -114,8 +137,14 @@
 </div>
 
 <div id="searchResultsComponent" style="margin-top: 10px;" class="ui-widget">
-    No results found yet !
+    Aucun résultat trouvé !
 </div>
+<script id="authorItemTemplate" type="text/x-jquery-tmpl">
+    <option value="{{= id}}"> {{= name}} </option>
+</script>
+<script id="contentTypeItemTemplate" type="text/x-jquery-tmpl">
+    <option value="{{= id}}"> {{= title}} </option>
+</script>
 <script id="contentResult" type="text/x-jquery-tmpl">
     <h3 style="color: #eb8f00;" class="ui-widget">Résultats de la recherche</h3>
 
---
+++ src/main/webapp/public/js/bbeeg/common/widgets/chainedSelect.js
@@ -0,0 +1,92 @@
+function ChainedSelect(_configuration){
+
+    var configuration = _configuration;
+
+    (function(){
+    }).call(this);
+
+    this.decorateSelectFieldWithChainedSelectConfiguration = function(selectField){
+        selectField.change(function(){
+           $(this).find("option:selected").each(function(){
+               addSelectedElement($(this));
+           });
+        });
+    };
+
+    function addSelectedElement(selectedOption){
+        // Updating targetFieldValue
+        var targetFieldValue = configuration.targetFieldForSelectedOption().val();
+        var currentDepth = 0;
+        if(targetFieldValue != ""){
+            targetFieldValue += "||";
+            currentDepth = targetFieldValue.split("||").length-1;
+        }
+        targetFieldValue += selectedOption.val();
+        configuration.targetFieldForSelectedOption().val(targetFieldValue);
+
+        // Updating displaySelectionTarget
+        $('<li class="search-choice"><span>'+selectedOption.text()+'</span><a href="#" class="search-choice-close"></a></li>').appendTo(configuration.displaySelectionTarget());
+
+        // Updating select content
+        var selectElement = selectedOption.parents('select').first();
+        selectElement.empty(); // Resetting select content
+        // Updating select content only if we aren't on the last depth
+        var ajaxUrlToCall = configuration.resolveAjaxUrlForOptionSelectedHandler().call(configuration, currentDepth+1, selectedOption);
+        if(ajaxUrlToCall != null){
+            $.get(ajaxUrlToCall, function(data) {
+                 $(data).each(function(){
+                     var resolvedOption = configuration.rowMappingHandler().call(null, currentDepth+1, this);
+                     selectElement.append($('<option></option>').val(resolvedOption.value).html(resolvedOption.label));
+                 });
+
+                // Saying chosen "hey you should update the select menu"
+                selectElement.trigger("liszt:updated");
+            }, "json");
+        } else {
+            // Refreshing chosen select menu to empty it
+            selectElement.trigger("liszt:updated");
+            // If we are on the last depth, let's hide the select menu
+            configuration.selectMenuContainer().hide();
+        }
+    };
+};
+ChainedSelect.Configuration = function(){
+    function ConfigurationConstructor() {
+        // ajaxUrlsPerDepth is a map [depth element starting by 0 => url] which will be used,
+        // by default, il the resolveAjaxUrlForOptionSelectedHandler.
+        // You can provide in your url special strings "{value}" and "{label}" if you want to pass
+        // parameters to your ajax url call
+        var _ajaxUrlsPerDepth;          // MANDATORY
+        this.ajaxUrlsPerDepth = function(__ajaxUrlsPerDepth){ if(__ajaxUrlsPerDepth==null){ return _ajaxUrlsPerDepth; } else { _ajaxUrlsPerDepth = __ajaxUrlsPerDepth; return this; } };
+        // Default implementation will use the ajaxUrlsPerDepth map
+        // to retrieve url for depth <depth> if <selectedOption> has been selected
+        // The méthod should return null if we are on the last depth
+        var _resolveAjaxUrlForOptionSelectedHandler = function(depth, selectedOption){
+            if(depth < this.ajaxUrlsPerDepth().length){
+                return this.ajaxUrlsPerDepth()[depth]
+                    .replace(ChainedSelect.Configuration.VALUE_REGEX, selectedOption.val())
+                    .replace(ChainedSelect.Configuration.LABEL_REGEX, selectedOption.text());
+            } else {
+                return null;
+            }
+        };
+        this.resolveAjaxUrlForOptionSelectedHandler = function(__resolveAjaxUrlForOptionSelectedHandler){ if(__resolveAjaxUrlForOptionSelectedHandler==null){ return _resolveAjaxUrlForOptionSelectedHandler; } else { _resolveAjaxUrlForOptionSelectedHandler = __resolveAjaxUrlForOptionSelectedHandler; return this; } };
+        // Default implementation will consider the jsonObjects returned will have
+        // a value and label fields to map into the lines, no matter the depth is
+        var _rowMappingHandler = function(depth, jsonObject){
+            return { value: jsonObject.value, label: jsonObject.label };
+        };
+        this.rowMappingHandler = function(__rowMappingHandler){ if(__rowMappingHandler==null){ return _rowMappingHandler; } else { _rowMappingHandler = __rowMappingHandler; return this; } };
+        // Field (hidden generaly) that will welcome the hierarchical path representing selected values
+        var _targetFieldForSelectedOption;  // MANDATORY
+        this.targetFieldForSelectedOption = function(__targetFieldForSelectedOption){ if(__targetFieldForSelectedOption==null){ return _targetFieldForSelectedOption; } else { _targetFieldForSelectedOption = __targetFieldForSelectedOption; return this; } };
+        // HTML Element where the selected hierarchy will be displayed
+        var _displaySelectionTarget;     // MANDATORY
+        this.displaySelectionTarget = function(__displaySelectionTarget){ if(__displaySelectionTarget==null){ return _displaySelectionTarget; } else { _displaySelectionTarget = __displaySelectionTarget; return this; } };
+        var _selectMenuContainer;
+        this.selectMenuContainer = function(__selectMenuContainer){ if(__selectMenuContainer==null){ return _selectMenuContainer; } else { _selectMenuContainer = __selectMenuContainer; return this; } };
+    }
+    return ConfigurationConstructor;
+}();
+ChainedSelect.Configuration.VALUE_REGEX = new RegExp("\\{value\\}", "ig");
+ChainedSelect.Configuration.LABEL_REGEX = new RegExp("\\{label\\}", "ig");
