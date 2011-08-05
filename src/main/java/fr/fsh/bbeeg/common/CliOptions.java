6791993171300e313698704d28000d2b1a51e736
Frédéric Camblor <fcamblor@gmail.com>
2011-08-05 20:01:45 +0200

    defined visioUrl parameter

--- src/main/java/fr/fsh/bbeeg/common/CliOptions.java
+++ src/main/java/fr/fsh/bbeeg/common/CliOptions.java
@@ -6,7 +6,14 @@ public class CliOptions {
     @Parameter(names = "-httpPort", description = "Http port used by Netty")
     private int httpPort = 8086;
 
+    @Parameter(names = "-visioUrl", description = "URL allowing to access EEG visualizer", required = true)
+    private String visioUrl;
+
     public int httpPort() {
         return this.httpPort;
+    }
+
+    public String visioUrl(){
+        return this.visioUrl;
     }
 }
