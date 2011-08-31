4f452ddfce6d00285d2421c6e9807d96ee0e10d2
Frédéric Camblor <fcamblor@gmail.com>
2011-08-31 11:44:26 +0200

    precised javadoc

--- src/main/java/jewas/http/data/HttpData.java
+++ src/main/java/jewas/http/data/HttpData.java
@@ -3,6 +3,7 @@ package jewas.http.data;
 /**
  * @author fcamblor
  * Interface representation for http data read in body content
+ * Every implementations should be immutable !
  */
 public interface HttpData {
     public boolean isCompleted();
