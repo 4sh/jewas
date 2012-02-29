2d1736d5598491349605610844f627b5bfe613ff
Christophe Armarolli <christophe.armarolli@4sh.fr>
2012-02-29 14:38:00 +0100

    Enlarge login column in database

--- src/main/resources/database/incremental/01_v1.0/004_create_user.sql
+++ src/main/resources/database/incremental/01_v1.0/004_create_user.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `USER` (
   `ID` bigint(20) NOT NULL auto_increment,
-  `LOGIN` varchar(10) NOT NULL,
+  `LOGIN` varchar(32) NOT NULL,
   `PASSWORD` varchar(10) NOT NULL,
   `NAME` varchar(64) NOT NULL,
   `SURNAME` varchar(64) NOT NULL,
