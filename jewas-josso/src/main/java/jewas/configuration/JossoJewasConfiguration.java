bea05af76973a9dc241df6d5362b2627410f8b09
Frédéric Camblor <fcamblor@gmail.com>
2012-09-27 16:04:39 +0200

    Provided additionnal JOSSO jewas configuration properties to retrieve various josso urls informations

--- jewas-josso/src/main/java/jewas/configuration/JossoJewasConfiguration.java
+++ jewas-josso/src/main/java/jewas/configuration/JossoJewasConfiguration.java
@@ -2,14 +2,51 @@ package jewas.configuration;
 
 /**
  * @author fcamblor
+ * Utility class allowing to retrieve special Jewas configuration properties relating to JOSSO
  */
 public class JossoJewasConfiguration {
     /**
      * The josso back url where josso will redirect back after a successful login
      */
-    public static final String JOSSO_BACK_URL_KEY = "josso.back.url";
+    private static final String JOSSO_BACK_URL_KEY = "josso.back.url";
+
+    /**
+     * Current josso partner app name
+     */
+    private static final String JOSSO_PARTNER_APP_NAME = "josso.partner.app.name";
+
+    /**
+     * Josso gateway server name
+     */
+    private static final String JOSSO_GATEWAY_SERVER_NAME = "josso.gateway.server.name";
+
+    /**
+     * Josso login url
+     */
+    private static final String JOSSO_GATEWAY_LOGIN_URL = "josso.login.url";
+
+    /**
+     * Josso logout url
+     */
+    private static final String JOSSO_GATEWAY_LOGOUT_URL = "josso.logout.url";
 
     public static String jossoBackUrl() {
         return JewasConfiguration.getValueOfKeyOrDefaultValue(JOSSO_BACK_URL_KEY, null);
+    }
+
+    public static String partnerAppName() {
+        return JewasConfiguration.getValueOfKeyOrDefaultValue(JOSSO_PARTNER_APP_NAME, null);
+    }
+
+    public static String gatewayServerName(){
+        return JewasConfiguration.getValueOfKeyOrDefaultValue(JOSSO_GATEWAY_SERVER_NAME, null);
+    }
+
+    public static String gatewayLoginUrl(){
+        return JewasConfiguration.getValueOfKeyOrDefaultValue(JOSSO_GATEWAY_LOGIN_URL, null);
+    }
+
+    public static String gatewayLogoutUrl(){
+        return JewasConfiguration.getValueOfKeyOrDefaultValue(JOSSO_GATEWAY_LOGOUT_URL, null);
     }
 }
