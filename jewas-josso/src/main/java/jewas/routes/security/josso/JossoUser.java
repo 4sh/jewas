cbbea57fc132325df62b91cab5494b3fb04e8e40
Frédéric Camblor <fcamblor@gmail.com>
2012-09-27 16:49:33 +0200

    Created utility class to set & retrieve josso user in session

---
+++ jewas-josso/src/main/java/jewas/routes/security/josso/JossoUser.java
@@ -0,0 +1,26 @@
+package jewas.routes.security.josso;
+
+import jewas.http.HttpRequest;
+import jewas.http.session.Sessions;
+
+import java.io.Serializable;
+
+/**
+ * @author fcamblor
+ * Utility class to set and retrieve JOSSO User in session
+ */
+public class JossoUser {
+    private static final String KEY_USER_INFOS = "jewas.user.infos";
+
+    public static Serializable get(HttpRequest request){
+        return Sessions.get(request).get(KEY_USER_INFOS);
+    }
+
+    public static void set(HttpRequest request, Serializable userInfos){
+        Sessions.get(request).set(KEY_USER_INFOS, userInfos);
+    }
+
+    public static boolean sessionOpened(HttpRequest request){
+        return get(request) != null;
+    }
+}
